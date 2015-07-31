3148e48492dd866ae8a5235a38075fffd57f863e
xingze <you@example.com>
2015-07-31 16:36:59 +0100

    read accelerometer data and present on the screen with time

--- Data/app/src/main/java/com/mycompany/data/Accelerometer.java
+++ Data/app/src/main/java/com/mycompany/data/Accelerometer.java
@@ -1,38 +1,79 @@
 package com.mycompany.data;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Vibrator;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
-public class Accelerometer extends ActionBarActivity {
+public class Accelerometer extends ActionBarActivity implements SensorEventListener{
+
+    private float x,y,z;
+    private float dx = 0,dy = 0,dz = 0;
+    private Long currentMillis;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private TextView currentX,currentY,currentZ,currentTime;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_accelerometer, menu);
-        return true;
-    }
+        currentX = (TextView) findViewById(R.id.currentX);
+        currentY = (TextView) findViewById(R.id.currentY);
+        currentZ = (TextView) findViewById(R.id.currentZ);
+        currentTime = (TextView) findViewById(R.id.currentTime);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) !=null ){
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
         }
 
-        return super.onOptionsItemSelected(item);
+        currentMillis = System.currentTimeMillis();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Long timeNow = (System.currentTimeMillis()-currentMillis)/1000;
+
+        currentX.setText(Float.toString(dx));
+        currentY.setText(Float.toString(dy));
+        currentZ.setText(Float.toString(dz));
+        currentTime.setText(Long.toString(timeNow));
+
+        dx = Math.abs(x - event.values[0]);
+        dy = Math.abs(y - event.values[1]);
+        dz = Math.abs(z - event.values[2]);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
