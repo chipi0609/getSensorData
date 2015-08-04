5737b11867c861956f483ab6625be287ae24b900
xingze <you@example.com>
2015-08-04 16:49:01 +0100

    add toast of the save button

--- Data/app/src/main/java/com/mycompany/data/AllSensors.java
+++ Data/app/src/main/java/com/mycompany/data/AllSensors.java
@@ -1,14 +1,19 @@
 package com.mycompany.data;
 
 import android.graphics.Typeface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
-public class AllSensors extends ActionBarActivity {
+public class AllSensors extends ActionBarActivity{
 
     Button start,stop,save;
 
@@ -35,5 +40,26 @@ public class AllSensors extends ActionBarActivity {
 
     }
 
+    public void start(View view) {
 
+    }
+
+    public void stop(View view) {
+
+    }
+
+    public void saveData(View view) {
+        Toast.makeText(AllSensors.this,"You have saved the sensors data!",Toast.LENGTH_SHORT).show();
+    }
+
+
+//    @Override
+//    public void onSensorChanged(SensorEvent event) {
+//
+//    }
+//
+//    @Override
+//    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//
+//    }
 }
